a2f12c9e49049dc7dd3d93582775c62f84327553
Stk <tharunkumar1197@gmail.com>
2023-11-17 22:59:51 +0530

    new set of ans

---
+++ Basics/q11.py
@@ -0,0 +1,18 @@
+# Write a code to get 2 integers A and N. Print the integer A, N times in separate line.
+
+# Input Description:
+# First line contains an integer A. Second line contains an Integer N.
+
+# Output Description:
+# Print the integer A, N times in a separate line.
+
+# Sample Input :
+# 2 3
+# Sample Output :
+# 2
+# 2
+# 2
+a,n = map(int,input().split())
+
+for i in range(0,n):
+    print(a)
