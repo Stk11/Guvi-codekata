331b6b43d69aed282b771d181664af14a72e8d75
Stk <tharunkumar1197@gmail.com>
2023-11-17 23:01:01 +0530

    ans

---
+++ Basics/q12.py
@@ -0,0 +1,16 @@
+# Write a code to get an integer N and print the sum of  values from 1 to N.
+
+# Input Description:
+# A single line contains an integer N.
+
+# Output Description:
+# Print the sum of values from 1 to N.
+
+# Sample Input :
+# 10
+# Sample Output :
+# 55
+
+a = int(input())
+x = (a*(a+1))/2
+print(x)
