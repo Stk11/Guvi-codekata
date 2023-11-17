f99c39b99a18c92c44b62dc863e27807a0f922cf
Stk <tharunkumar1197@gmail.com>
2023-11-17 23:09:31 +0530

    area

---
+++ Basics/q15.py
@@ -0,0 +1,10 @@
+# Given base(B) and height(H) of a triangle find its area.
+# Input Size : N <= 1000000
+# Sample Testcase :
+# INPUT
+# 2 4
+# OUTPUT
+# 4
+
+a,b = map(int,input().split())
+print((a*b)/2)
