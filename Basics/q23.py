12a8589d0b5733f35fa2ef1c6b7e46a860bb2238
Stk <tharunkumar1197@gmail.com>
2023-12-01 16:53:31 +0530

    sum

---
+++ Basics/q23.py
@@ -0,0 +1,26 @@
+# Write a code get an integer number as input and print the sum of the digits.
+
+# Input Description:
+# A single line containing an integer.
+
+# Output Description:
+# Print the sum of the digits of the integer.
+
+# Sample Input :
+# 124
+# Sample Output :
+# 7
+
+# a = list(input())
+# x = 0
+# for i in range(0,len(a)):
+#     x += int(a[i])
+    
+# print(x)
+
+a = list(input())
+x = 0
+for i in a:
+    x += int(i)
+    
+print(x)
