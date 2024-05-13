022d612d2d3cf91225125ce887fb4e1fa387eaa4
Stk <tharunkumar1197@gmail.com>
2024-05-13 10:51:56 +0530

    engine number

--- Strings/q2.py
+++ Strings/q2.py
@@ -1,4 +1,4 @@
-# In XYZ country there is rule that car’s engine no. depends upon car’ number plate. Engine no is sum of all the integers present on car’s Number plate.The issuing authority has hired you in order to provide engine no. to the cars.Your task is to develop an algorithm which takes input as in form of string(Number plate) and gives back
+# In XYZ country there is rule that car’s engine no. depends upon car’ number s. Engine no is sum of all the integers present on car’s Number s.The issuing authority has hired you in order to provide engine no. to the cars.Your task is to develop an algorithm which takes input as in form of string(Number s) and gives back
 
 # Engine number.
 
@@ -12,3 +12,19 @@
 # HR05-AA-2669
 # Sample Output :
 # 28
+
+s = list(input())
+
+num = 0
+for i in range((len(s))):
+    if s[i].isdigit():
+        num += int(s[i])
+print(num)
+# count = [0,1,2,3,4,5,6,7,8,9]
+# num = 0
+# for ele in s:
+#     if int(ele) in count:
+#         print(int(ele))
+#         num += int(ele)
+#         print(num)
+# print(num)
